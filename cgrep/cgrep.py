375a7b7ee2d8af680dc38a9a7d03d9681fcf2dcc
Eugenio Dols <eudols@users.noreply.github.com>
2015-04-03 13:22:50 -0700

    Added getopt

--- cgrep/cgrep.py
+++ cgrep/cgrep.py
@@ -1,7 +1,32 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import sys
+import sys, getopt
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
-print sys.argv
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+print(sys.argv)
+
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print('test.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('test.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print('Input file is "', inputfile)
+   print('Output file is "', outputfile)
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == "__main__":
+    main(sys.argv[1:])
