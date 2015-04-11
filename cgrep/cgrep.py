a3dfa85a9a83f1e8081713b7a81e467ac4f9172c
Eugenio Dols <eudols@users.noreply.github.com>
2015-04-10 23:06:26 -0700

    First really complete and working version of cgrep in Python

--- cgrep/cgrep.py
+++ cgrep/cgrep.py
@@ -6,13 +6,31 @@ import sys, getopt, re, os
 #print('Argument List:', str(sys.argv))
 #print(sys.argv)
 
+def file_search(fullpath, pattern):
+    regObj = re.compile(pattern)
+    lineNumber = 0
+    try:
+        f = open(fullpath, 'r')
+    except IOError:
+        print('*** Could not open', fullpath)
+    else:
+        #print(f)
+        for line in f:
+            lineNumber += 1
+            if regObj.search(line):
+                print(fullpath, ':', lineNumber, ' ', line.rstrip('\n'), sep='')
+        f.close()
+    
+
 def explore(filepattern, pattern):
     regObj = re.compile(filepattern)
-    for root, dirs, fnames in os.walk('.'):
-        for fname in fnames:
-            print('filename ', root, '/', fname, sep='')
-            if regObj.match(fname):
-                print('file', fname, 'matches')
+    for root, dirs, files in os.walk('.'):
+        for fname in files:
+            #print('filename ', root, '/', fname, sep='')
+            if regObj.search(fname):
+                #print('file', fname, 'matches')
+                fullpath = os.path.join(root, fname)
+                file_search(fullpath, pattern)
 
 def usage():
     print('Usage: cgrep [-mp] [-f <file regexp>] <regexp>')
@@ -25,8 +43,15 @@ def usage():
 def main(argv):
     pattern     = ''
     filepattern = '\.(h|c|cc|cpp)$'; # default to source files
+    
+    # if no arguments, print usage and exit
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit()
+
+    # Let's retrieve the arguments
     try:
-        opts, args = getopt.getopt(argv,"cmpf:o:")
+        opts, args = getopt.getopt(argv,"cmpf:")
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -43,11 +68,12 @@ def main(argv):
         elif opt in ("-f"):
             filepattern = arg
 
-    print('File pattern is "', filepattern)
-    print('arg1 is ', str(sys.argv[len(sys.argv) - 1]))
-    print('arg is "', arg)
-    pattern = str(sys.argv[len(sys.argv) - 1]) # pattern is the last argument
-    print('pattern = ', pattern)
+    #print('File pattern is ', filepattern)
+    #print('arg1 is ', str(sys.argv[len(sys.argv) - 1]))
+    #print('arg is "', arg)
+
+    pattern = str(sys.argv[len(sys.argv) - 1]) # pattern is last argument
+    #print('pattern = ', pattern)
     explore(filepattern, pattern)
 
 # Standard boilerplate to call the main() function to begin
